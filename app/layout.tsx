895b42fac30a8744d4737274b6d013417a0f0b06
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2026-04-04 03:16:44 +0530

    fixed default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-background text-foreground">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange={false}
         >
